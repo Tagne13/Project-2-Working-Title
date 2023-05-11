0ce7feb6fe6d8a7bc9b977effb38a2a3109271ae
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-05-11 19:34:09 -0400

    seeds file logic correction.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,18 @@
 const sequelize = require("../config/connection");
 const { User, Review, Comment } = require("../models");
-
 const userData = require("./userData.json");
 const reviewData = require("./reviewData.json");
 const commentData = require("./commentData.json");
+
+const getRandomUserId = (users) => {
+  const randomIndex = Math.floor(Math.random() * users.length);
+  return users[randomIndex].id;
+};
+
+const getRandomReviewId = (reviews) => {
+  const randomIndex = Math.floor(Math.random() * reviews.length);
+  return reviews[randomIndex].id;
+};
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,17 +22,27 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const review of reviewData) {
-    await Review.create({
-      ...review,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  const reviews = await Review.bulkCreate(reviewData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const review of reviews) {
+    const randomUserId = getRandomUserId(users);
+
+    await review.update({
+      user_id: randomUserId,
     });
   }
 
   for (const comment of commentData) {
+    const randomUserId = getRandomUserId(users);
+    const randomReviewId = getRandomReviewId(reviews);
+
     await Comment.create({
       ...comment,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      user_id: randomUserId,
+      review_id: randomReviewId,
     });
   }
 
