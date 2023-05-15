73404d7288ca809c84bb18347225b028b1dd9241
El Jefe <Tagne13@gmail.com>
2023-05-15 18:50:20 -0400

    modified user routes to reflect acceptance criteria

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User  = require('../../models/user');
+const { User } = require('../../models');
 
 // GET all users
 router.get('/', async (req, res) => {
@@ -28,32 +28,56 @@ router.get('/:id', async (req, res) => {
 // CREATE a new user
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+        const userData = await User.create(req.body);
+        
+        req.sessio.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
         });
-        res.status(200).json(userData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-// UPDATE a user by their id value
-router.put('/:id', async (req, res) => {
+// Route to login a user
+router.post('/login', async (req, res) => {
     try {
-        const userData = await User.update(req.body, {
-            where: {
-                id: req.params.id,
-            },
-        });
-        if (!userData[0]) {
-            res.status(404).json({ message: 'No user found with this id!' });
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email address or password, please try again.' });
             return;
         }
-        res.status(200).json(userData);
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email address or password, please try again.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You have successfully logged in!' });
+        });
+
     } catch (err) {
-        res.status(500).json(err);
+        res.status(400).json(err);
+    }
+});
+
+// Route to logout a user
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(200).end('You have successfully logged out!');
+        });
+    } else {
+        res.status(404).end();
     }
 });
 
